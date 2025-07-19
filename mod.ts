83c895df78113c5d4e3a6397f17bafdc31163f68
James R. Cogley <rick.cogley@esolia.co.jp>
2025-07-19 18:26:53 +0900

    chore(release): bump version to 2.11.4

--- mod.ts
+++ mod.ts
@@ -5,7 +5,7 @@
  *
  * | **Nagare** | **Specifications** |
  * | :------- | :------- |
- * | Version | 2.11.3 |
+ * | Version | 2.11.4 |
  * | Repository | {@link https://github.com/RickCogley/nagare} |
  * | JSR Package | {@link https://jsr.io/@rick/nagare} |
  * | Documentation | {@link https://nagare.esolia.deno.net} |
